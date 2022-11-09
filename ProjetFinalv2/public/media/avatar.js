a168ca2c64151841ebb114f4e55f101a0d686044
stomruk <s.tomruk35@gmail.com>
2022-11-09 09:07:51 +0100

    avatar test

--- ProjetFinalv2/public/media/avatar.js
+++ ProjetFinalv2/public/media/avatar.js
@@ -4,8 +4,9 @@ let Selectedtab = 'head-options'
 let Heads = document.getElementsByClassName('head-options')
 let Hairs = document.getElementsByClassName('hair-options')
 let Eyes = document.getElementsByClassName('eyes-options')
+let Mouths = document.getElementsByClassName('mouth-options')
 
-let Alltabs = [Heads, Hairs, Eyes]
+let Alltabs = [Heads, Hairs, Eyes, Mouths]
 
 let colors = document.getElementsByClassName('grid-color')
 
@@ -26,23 +27,36 @@ document.getElementById('head-btn').addEventListener('click', e=>{
         num++;
     })
 })
+
 document.getElementById('hair-btn').addEventListener('click', e=>{
     Selectedtab = Hairs
     changeTab()
     num = 0
     Array.from(colors).forEach(col => {
         document.getElementById(col.id).style.gridRow = "1 / 2"
-        document.getElementById(col.id).style.backgroundColor = hairCol[num]
+        document.getElementById(col.id).style.backgroundColor = hairColors[num]
         num++;
     })
 })
+
 document.getElementById('eyes-btn').addEventListener('click', e=>{
     Selectedtab = Eyes
     changeTab()
     num = 0
     Array.from(colors).forEach(col => {
         document.getElementById(col.id).style.gridRow = "1 / 2"
-        document.getElementById(col.id).style.backgroundColor = hairCol[num]
+        document.getElementById(col.id).style.backgroundColor = hairColors[num]
+        num++;
+    })
+})
+
+document.getElementById('mouth-btn').addEventListener('click', e=>{
+    Selectedtab = Mouths
+    changeTab()
+    num = 0
+    Array.from(colors).forEach(col => {
+        document.getElementById(col.id).style.gridRow = "1 / 3"
+        document.getElementById(col.id).style.backgroundColor = skinColor[num]
         num++;
     })
 })
@@ -56,6 +70,9 @@ function changeTab(){
     }
     else if (Selectedtab === Eyes){
         sect = 'eyes'
+    }
+    else if (Selectedtab === Mouths){
+        sect = 'mouth'
     }
     Array.from(Alltabs).forEach(t =>{
         Array.from(t).forEach(f=>{
@@ -74,6 +91,9 @@ function changeHead(type,color){
 }
 function changeEyes(type,color){
     document.getElementById('eyes').style.backgroundImage = 'url(../media/avatar/eyes/'+type+'/'+color+'.png)'
+}
+function changeMouth(type,color){
+    document.getElementById('mouth').style.backgroundImage = 'url(../media/avatar/mouth/'+type+'/'+color+'.png)'
 }
 
 
--- ProjetFinalv2/src/Controller/AvatarController.php
+++ ProjetFinalv2/src/Controller/AvatarController.php
@@ -6,6 +6,7 @@ use App\Repository\EyesRepository;
 use App\Repository\HairColorRepository;
 use App\Repository\HairRepository;
 use App\Repository\HeadRepository;
+use App\Repository\MouthRepository;
 use App\Repository\SkinColorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AvatarController extends AbstractController
 {
     #[Route('/avatar', name: 'app_avatar')]
-    public function index(HeadRepository $headRepository, SkinColorRepository $skinColorRepository, HairRepository $hairRepository,HairColorRepository $hairColorRepository,EyesRepository $eyesRepository): Response
+    public function index(HeadRepository $headRepository, SkinColorRepository $skinColorRepository, HairRepository $hairRepository,HairColorRepository $hairColorRepository,EyesRepository $eyesRepository, MouthRepository $mouthRepository): Response
     {
         return $this->render('profil/avatar.html.twig',
             [
@@ -23,6 +24,7 @@ class AvatarController extends AbstractController
             'hairs' => $hairRepository->findAll(),
             'hairColors' => $hairColorRepository->findAll(),
             'eyes' => $eyesRepository->findAll(),
+            'mouths' => $mouthRepository->findAll(),
             ]);
     }
 
